2d719f598378a6f431ac6c0470a839c9c1e43e3b
chyok <chyok@hotmail.com>
2024-05-15 22:46:52 +0800

    Update button

--- ten_drops/button.py
+++ ten_drops/button.py
@@ -10,55 +10,40 @@ NormalFont = Font(FONT_PATH, size=65)
 NormalFont.set_italic(True)
 
 
-class StartButton(Sprite):
+class BaseButton(Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
         self._update_color(white)
 
+    def mouse_hover(self):
+        self._update_color(grey)
+
+    def mouse_leave(self):
+        self._update_color(white)
+
+    def _update_color(self, color):
+        raise NotImplementedError
+
+
+class StartButton(BaseButton):
     def _update_color(self, color):
         self.image = NormalFont.render("start", True, color)
         self.rect = self.image.get_rect()
         self.rect.x = PLAYGROUND_LENGTH + PLAYGROUND_OFFSET * 2
         self.rect.y = PLAYGROUND_OFFSET * 13.5
 
-    def mouse_hover(self):
-        self._update_color(grey)
 
-    def mouse_leave(self):
-        self._update_color(white)
-
-
-class AboutButton(Sprite):
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self._update_color(white)
-
+class AboutButton(BaseButton):
     def _update_color(self, color):
         self.image = NormalFont.render("about", True, color)
         self.rect = self.image.get_rect()
         self.rect.x = PLAYGROUND_LENGTH + PLAYGROUND_OFFSET * 2
         self.rect.y = PLAYGROUND_OFFSET * 15.5
 
-    def mouse_hover(self):
-        self._update_color(grey)
 
-    def mouse_leave(self):
-        self._update_color(white)
-
-
-class ExitButton(Sprite):
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self._update_color(white)
-
+class ExitButton(BaseButton):
     def _update_color(self, color):
         self.image = NormalFont.render("exit", True, color)
         self.rect = self.image.get_rect()
         self.rect.x = PLAYGROUND_LENGTH + PLAYGROUND_OFFSET * 2
         self.rect.y = PLAYGROUND_OFFSET * 17.5
-
-    def mouse_hover(self):
-        self._update_color(grey)
-
-    def mouse_leave(self):
-        self._update_color(white)
